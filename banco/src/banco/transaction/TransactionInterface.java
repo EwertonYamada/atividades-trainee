d8058df43e58eea93b819310e1cdfa8cadb99c26
EwertonYamada <ewerton_yamada@hotmail.com>
2026-02-25 11:30:58 -0300

    removendo atividade

--- banco/src/banco/transaction/TransactionInterface.java
+++
@@ -1,5 +0,0 @@
-package terminal.transaction;
-
-public interface TransactionInterface {
-
-}
--- banco/src/banco/utils/enums/TransactionType.java
+++
@@ -1,7 +0,0 @@
-package terminal.utils.enums;
-
-public enum TransactionType {
-    SAQUE,
-    DEPOSITO,
-    TRANSFERENCIA;
-}
